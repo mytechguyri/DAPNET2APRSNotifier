6141d37446a583527a618996096c5ded15f6f181
John Tetreault (WA1OKB) <46641218+mytechguyri@users.noreply.github.com>
2024-02-24 14:50:24 -0500

    Create install.sh
    Simple bash installer script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo cp ./DAPNET2APRSNotifier /usr/local/bin/DAPNET2APRSNotifier
+sudo chown mmdvm:mmdvm /usr/local/bin/DAPNET2APRSNotifier
+sudo chmod +x /usr/local/bin/DAPNET2APRSNotifier
+sudo cp ./dapnet2aprs /etc/dapnet2aprs
+sudo chown mmdvm:mmdvm /etc/dapnet2aprs
+sudo cp ./dapnet2aprs.service /usr/lib/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable dapnet2aprs
