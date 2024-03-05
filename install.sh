fd149b24db6d0e609c2e4c91156a37ce8eb2059f
John Tetreault (WA1OKB) <46641218+mytechguyri@users.noreply.github.com>
2024-03-05 10:50:57 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo chown mmdvm:mmdvm /usr/local/bin/DAPNET2APRSNotifier
 sudo chmod +x /usr/local/bin/DAPNET2APRSNotifier
 sudo cp ./dapnet2aprs /etc/dapnet2aprs
 sudo chown mmdvm:mmdvm /etc/dapnet2aprs
-sudo cp ./dapnet2aprs.service /usr/lib/systemd/system
+sudo cp ./dapnet2aprs.service /lib/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable dapnet2aprs
 sudo nano /etc/dapnet2aprs
